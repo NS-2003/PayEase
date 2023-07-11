ef23c29a3bd479f88cc823b21e400641173b2df7
NS <94551491+NS-2003@users.noreply.github.com>
2023-07-11 15:50:41 +0530

    Add files via upload

---
+++ Day 3/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/Resgistration/LoginSignup';
+import LandingPage from './Components/LandingPage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+      {/* <LandingPage /> */}
+    </div>
+  );
+}
+
+export default App;
